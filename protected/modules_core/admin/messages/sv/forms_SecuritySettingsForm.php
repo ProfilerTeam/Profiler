da6c39e2cb07e9d65d7f5b17c3b9e58713ce9b8e
zakdoesgaming <zakdoesgaming@gmail.com>
2015-05-26 13:55:24 +0100

    Added Swedish translation for SecuritySettingsForm.

--- protected/modules_core/admin/messages/sv/forms_SecuritySettingsForm.php
+++ protected/modules_core/admin/messages/sv/forms_SecuritySettingsForm.php
@@ -1,4 +1,4 @@
 <?php
 return array (
-  'Super Admins can delete each content object' => '',
+  'Super Admins can delete each content object' => 'Super Administratörer kan ta bort varje innehållsobjekt',
 );
