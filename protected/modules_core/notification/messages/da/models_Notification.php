58ae656d53f332f78792465d62bb058141e69cd5
Theis <TheisCHR@live.dk>
2016-02-24 14:31:21 +0100

    Update models_Notification.php
    Yup, zak sucks at danish.

--- protected/modules_core/notification/messages/da/models_Notification.php
+++ protected/modules_core/notification/messages/da/models_Notification.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'Could not determine redirect url for this kind of source object!' => 'Kunne ikke bestemme omdirigering url for denne form for kildeobjektet.',
-  'Could not load notification source object to redirect to!' => 'Kunne ikke indlæse meddelelse kildeobjekt at omdirigere til!',
+  'Could not determine redirect url for this kind of source object!' => 'Kunne ikke bestemme omdirigerings adresse for denne form for kildeobjektet.',
+  'Could not load notification source object to redirect to!' => 'Kunne ikke indlæse notifikations kilde objekt til at omdirigere til!',
 );
